09a9808406965a9b4f6727f3acbe2ac10418c40a
rjz14 <53631056+rjz14@users.noreply.github.com>
2020-03-09 13:24:10 -0400

    Add files via upload
    tested validity checks on state and outcome arguments of best.R function

--- best.R
+++ best.R
@@ -1,14 +1,20 @@
 #This function returns the hospital with the lowest mortality rate associated with a given outcome and state.
 best <- function(state, outcome) {
         ## Read outcome data
-        outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-        outcome[, 11] <- as.numeric(outcome[, 11])
+        outcomeData <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+        outcomeData[, 11] <- as.numeric(outcomeData[, 11])
         
         ## Check that state and outcome are valid
-        if sum(state == outcome[,7]) > 0 {
-                message("invalid state")
-                error
+        if (sum(state == outcomeData[,7]) == 0) {
+                #message("invalid state")
+                stop("invalid state")
         }
+        
+        if (sum(outcome == c("heart attack", "heart failure", "pneumonia")) == 0) {
+                #message("invalid outcome")
+                stop("invalid outcome")
+        }
+        
         
         ## Return hospital name in that state with lowest 30-day death
         ## rate
