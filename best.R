9715a39113203815e95b75966368947f73807f92
rjz14 <53631056+rjz14@users.noreply.github.com>
2020-03-10 13:05:02 -0400

    Add files via upload
    continuing best.R

--- best.R
+++ best.R
@@ -3,9 +3,11 @@ best <- function(state, outcome) {
         ## Read outcome data
         outcomeData <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
         outcomeData[, 11] <- as.numeric(outcomeData[, 11])
+        outcomeData[, 17] <- as.numeric(outcomeData[, 17])
+        outcomeData[, 23] <- as.numeric(outcomeData[, 23])
         
         ## Check that state and outcome are valid
-        if (sum(state == outcomeData[,7]) == 0) {
+        if (sum(state == outcomeData[, 7]) == 0) {
                 #message("invalid state")
                 stop("invalid state")
         }
@@ -15,7 +17,17 @@ best <- function(state, outcome) {
                 stop("invalid outcome")
         }
         
-        
+        if (outcome == "heart attack") {
+                noNAOutcomeData <- cbind(outcomeData[, 2][!is.na(outcomeData[, 11])], outcomeData[, 11][!is.na(outcomeData[, 11])])
+                
+        } else if (outcome == "heart failure") {
+                noNAOutcomeData <- cbind(outcomeData[, 2][!is.na(outcomeData[, 17])], outcomeData[, 17][!is.na(outcomeData[, 17])])
+        } else if (outcome == "pneumonia") {
+                noNAOutcomeData <- cbind(outcomeData[, 2][!is.na(outcomeData[, 23])], outcomeData[, 23][!is.na(outcomeData[, 23])])
+        }
+        minMortRate <- min(noNAOutcomeData[, 2])
+        bestHospitalsBoolean <- minMortRate == noNAOutcomeData[, 2]
+        bestHospitals <- noNAOutcomeData[, 1][bestHospitalsBoolean]
         ## Return hospital name in that state with lowest 30-day death
         ## rate
 }
